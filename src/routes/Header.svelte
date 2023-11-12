034ec449efe40c3daa68df7158280fe699de099b
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2023-11-12 17:11:30 +0100

    ad why ? page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -60,6 +60,13 @@
 				>
 					<a href="/question4">Question 4</a>
 				</li>
+				<li
+					aria-current={$page.url.pathname === "/pourquoi"
+						? "page"
+						: undefined}
+				>
+					<a href="/pourquoi">Pourquoi ?</a>
+				</li>
 			</ul>
 			<svg viewBox="0 0 2 3" aria-hidden="true">
 				<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
@@ -104,6 +111,13 @@
 						: undefined}
 				>
 					<a href="/question4" on:click={() => (open = false)}>Question 4</a>
+				</li>
+				<li
+					aria-current={$page.url.pathname === "/pourquoi"
+						? "page"
+						: undefined}
+				>
+					<a href="/pourquoi" on:click={() => (open = false)}>Pourquoi ?</a>
 				</li>
 			</ul>
 		</div>
---
+++ src/routes/pourquoi/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+    import lighthouse from '$lib/assets/lighthouse.webp'
+    import ecoindex from '$lib/assets/ecoindex.webp'
+</script>
+
+<svelte:head>
+	<title>Pourquoi ?</title>
+	<meta name="description" content="Développement durable - Pourquoi ?" />
+</svelte:head>
+
+<section>
+    <div class="question">
+		<div class="left">
+			<h2>Pourquoi ?</h2>
+		</div>
+		<div class="right">
+            <div>
+                <p>
+                    Ce site est conçu et développé de manière responsable. Une score de 100/100 à toutes les étapes de Lighthouse.
+                </p>
+                <img src={lighthouse} alt="lighthouse" width="300" height="49"/>
+                <p>
+                    Et une score EcoIndex A.
+                </p>
+                <img src={ecoindex} alt="ecoindex score" width="400" height="84"/>
+            </div>
+		</div>
+	</div>
+</section>
+
+<style>
+    section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	.question {
+		display: flex;
+		width: 100%;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+	}
+
+	.left {
+		flex: 0.3;
+	}
+
+	.right{
+		flex: 1;
+	}
+
+	@media (max-width: 480px) {
+		.question {
+			flex-direction: column;
+		}
+
+        img {
+            max-width: 100%;
+        }
+	}
+</style>
